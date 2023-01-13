0d8a50c9239f600e8ee244d583c443d79403364c
Denis Vakulenko <101655241+woofmgn@users.noreply.github.com>
2023-01-13 19:18:21 +0500

    refactor: adaptive

--- src/scripts/modules/toggleQtySwiperSlide.js
+++ src/scripts/modules/toggleQtySwiperSlide.js
@@ -1,144 +1,89 @@
 import { Swiper } from "../libs.js";
 
+const SwiperTeamSeggings = {
+  swiperSelector: ".um-team__swiper",
+  paginSelector: ".um-team__swiper-pagination",
+  btnNextSelector: ".um-team__swiper-btn_type_next",
+  btnPrevSelector: ".um-team__swiper-btn_type_prev",
+};
+
+const SwiperCertificateSeggings = {
+  swiperSelector: ".um-certificate__swiper",
+  paginSelector: ".um-certificate__swiper-pagination",
+  btnNextSelector: ".um-certificate__swiper-btn_type_next",
+  btnPrevSelector: ".um-certificate__swiper-btn_type_prev",
+};
+
+const createSwiper = (
+  newQty,
+  { swiperSelector, paginSelector, btnNextSelector, btnPrevSelector }
+) => {
+  const swiper = new Swiper(swiperSelector, {
+    slidesPerView: newQty,
+    spaceBetween: 30,
+    slidesPerGroup: 1,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: paginSelector,
+      clickable: true,
+    },
+    navigation: {
+      nextEl: btnNextSelector,
+      prevEl: btnPrevSelector,
+    },
+  });
+
+  return swiper;
+};
+
 export const toggleQtySwiperSlideTeam = () => {
   let qtySlide = 3;
-  if (window.innerWidth > 769) {
+
+  if (window.innerWidth > 1023) {
     qtySlide = 3;
-    const swiperTeam = new Swiper(".um-team__swiper", {
-      slidesPerView: qtySlide,
-      spaceBetween: 30,
-      slidesPerGroup: 1,
-      loop: true,
-      loopFillGroupWithBlank: true,
-      pagination: {
-        el: ".um-team__swiper-pagination",
-        clickable: true,
-      },
-      navigation: {
-        nextEl: ".um-team__swiper-btn_type_next",
-        prevEl: ".um-team__swiper-btn_type_prev",
-      },
-    });
+    const newSwiperTeam = createSwiper(qtySlide, SwiperTeamSeggings);
 
-    return swiperTeam;
-  } else if (window.innerWidth < 769 && window.innerWidth > 450) {
+    return newSwiperTeam;
+  } else if (window.innerWidth < 1024 && window.innerWidth > 768) {
     qtySlide = 2;
-    const swiperTeam = new Swiper(".um-team__swiper", {
-      slidesPerView: qtySlide,
-      spaceBetween: 20,
-      slidesPerGroup: 1,
-      loop: true,
-      loopFillGroupWithBlank: true,
-      pagination: {
-        el: ".um-team__swiper-pagination",
-        clickable: true,
-      },
-      navigation: {
-        nextEl: ".um-team__swiper-btn_type_next",
-        prevEl: ".um-team__swiper-btn_type_prev",
-      },
-    });
+    const newSwiperTeam = createSwiper(qtySlide, SwiperTeamSeggings);
 
-    return swiperTeam;
-  } else if (window.innerWidth < 450) {
+    return newSwiperTeam;
+  } else if (window.innerWidth < 768) {
     qtySlide = 1;
-    const swiperTeam = new Swiper(".um-team__swiper", {
-      slidesPerView: qtySlide,
-      spaceBetween: 30,
-      slidesPerGroup: 1,
-      loop: true,
-      loopFillGroupWithBlank: true,
-      pagination: {
-        el: ".um-team__swiper-pagination",
-        clickable: true,
-      },
-      navigation: {
-        nextEl: ".um-team__swiper-btn_type_next",
-        prevEl: ".um-team__swiper-btn_type_prev",
-      },
-    });
+    const newSwiperTeam = createSwiper(qtySlide, SwiperTeamSeggings);
 
-    return swiperTeam;
+    return newSwiperTeam;
   }
 };
 
 export const toggleQtySwiperSlideCertificate = () => {
   let qtySlide = 5;
-  if (window.innerWidth > 769) {
+
+  if (window.innerWidth > 1023) {
     qtySlide = 5;
-    const swiperCertificate = new Swiper(".um-certificate__swiper", {
-      slidesPerView: qtySlide,
-      spaceBetween: 30,
-      slidesPerGroup: 1,
-      loop: true,
-      loopFillGroupWithBlank: true,
-      pagination: {
-        el: ".um-certificate__swiper-pagination",
-        clickable: true,
-      },
-      navigation: {
-        nextEl: ".um-certificate__swiper-btn_type_next",
-        prevEl: ".um-certificate__swiper-btn_type_prev",
-      },
-    });
+    const newSwiperCertificate = createSwiper(
+      qtySlide,
+      SwiperCertificateSeggings
+    );
 
-    return swiperCertificate;
-  } else if (window.innerWidth < 769 && window.innerWidth > 600) {
+    return newSwiperCertificate;
+  } else if (window.innerWidth < 1024 && window.innerWidth > 767) {
     qtySlide = 4;
-    const swiperCertificate = new Swiper(".um-certificate__swiper", {
-      slidesPerView: qtySlide,
-      spaceBetween: 20,
-      slidesPerGroup: 1,
-      loop: true,
-      loopFillGroupWithBlank: true,
-      pagination: {
-        el: ".um-certificate__swiper-pagination",
-        clickable: true,
-      },
-      navigation: {
-        nextEl: ".um-certificate__swiper-btn_type_next",
-        prevEl: ".um-certificate__swiper-btn_type_prev",
-      },
-    });
+    const newSwiperCertificate = createSwiper(
+      qtySlide,
+      SwiperCertificateSeggings
+    );
 
-    return swiperCertificate;
-  } else if (window.innerWidth < 600 && window.innerWidth > 500) {
-    qtySlide = 3;
-    const swiperCertificate = new Swiper(".um-certificate__swiper", {
-      slidesPerView: qtySlide,
-      spaceBetween: 20,
-      slidesPerGroup: 1,
-      loop: true,
-      loopFillGroupWithBlank: true,
-      pagination: {
-        el: ".um-certificate__swiper-pagination",
-        clickable: true,
-      },
-      navigation: {
-        nextEl: ".um-certificate__swiper-btn_type_next",
-        prevEl: ".um-certificate__swiper-btn_type_prev",
-      },
-    });
-
-    return swiperCertificate;
-  } else if (window.innerWidth < 500) {
+    return newSwiperCertificate;
+  } else if (window.innerWidth < 768 && window.innerWidth > 300) {
     qtySlide = 2;
-    const swiperCertificate = new Swiper(".um-certificate__swiper", {
-      slidesPerView: qtySlide,
-      spaceBetween: 19,
-      slidesPerGroup: 1,
-      loop: true,
-      loopFillGroupWithBlank: true,
-      pagination: {
-        el: ".um-certificate__swiper-pagination",
-        clickable: true,
-      },
-      navigation: {
-        nextEl: ".um-certificate__swiper-btn_type_next",
-        prevEl: ".um-certificate__swiper-btn_type_prev",
-      },
-    });
+    const newSwiperCertificate = createSwiper(
+      qtySlide,
+      SwiperCertificateSeggings
+    );
 
-    return swiperCertificate;
+    return newSwiperCertificate;
   }
 };
