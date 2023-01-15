afb058a40d17f7963d9aa96ec36b3bf72689d9cc
Denis Vakulenko <101655241+woofmgn@users.noreply.github.com>
2023-01-15 14:12:35 +0500

    fix: slider

--- src/scripts/modules/toggleImages.js
+++ src/scripts/modules/toggleImages.js
@@ -1,11 +1,15 @@
 const aboutImgSelector = document.querySelector(".um-about-company__image");
+const promoTitle = document.querySelector(".js-change-title");
 
 export const toggleImages = () => {
   if (window.innerWidth > 1023) {
     aboutImgSelector.src = aboutImgSelector.dataset.imageDesctop;
+    promoTitle.textContent = promoTitle.dataset.textDesctop;
   } else if (window.innerWidth <= 1023 && window.innerWidth > 400) {
     aboutImgSelector.src = aboutImgSelector.dataset.imageTablet;
+    promoTitle.textContent = promoTitle.dataset.textDesctop;
   } else if (window.innerWidth <= 400) {
     aboutImgSelector.src = aboutImgSelector.dataset.imageDesctop;
+    promoTitle.textContent = promoTitle.dataset.textMobile;
   }
 };
