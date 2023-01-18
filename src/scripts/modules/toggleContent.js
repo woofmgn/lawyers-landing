3d45984bd258537057ac0701983600ceca41aa37
Denis Vakulenko <101655241+woofmgn@users.noreply.github.com>
2023-01-18 21:43:42 +0500

    fix

--- src/scripts/modules/toggleContent.js
+++ src/scripts/modules/toggleContent.js
@@ -4,19 +4,19 @@ const certificateSubtitle = document.querySelector(".js-certificate__subtitle");
 
 export const toggleContent = () => {
   if (window.innerWidth > 1023) {
-    if (window.location.href === "http://localhost:3000/" || "/") {
+    if (window.location.href === "http://localhost:3000/") {
       aboutImgSelector.src = aboutImgSelector.dataset.imageDesctop;
       promoTitle.textContent = promoTitle.dataset.textDesctop;
       certificateSubtitle.textContent = certificateSubtitle.dataset.textPc;
     }
   } else if (window.innerWidth <= 1023 && window.innerWidth > 400) {
-    if (window.location.href === "http://localhost:3000/" || "/") {
+    if (window.location.href === "http://localhost:3000/") {
       aboutImgSelector.src = aboutImgSelector.dataset.imageTablet;
       promoTitle.textContent = promoTitle.dataset.textDesctop;
       certificateSubtitle.textContent = certificateSubtitle.dataset.textTablet;
     }
   } else if (window.innerWidth <= 400) {
-    if (window.location.href === "http://localhost:3000/" || "/") {
+    if (window.location.href === "http://localhost:3000/") {
       aboutImgSelector.src = aboutImgSelector.dataset.imageDesctop;
       promoTitle.textContent = promoTitle.dataset.textMobile;
       certificateSubtitle.textContent = certificateSubtitle.dataset.textTablet;
